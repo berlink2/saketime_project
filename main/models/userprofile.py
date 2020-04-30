bfd88a9de55bd0fc27e1e3539a93dfe7f495e850
berlink2 <berlink.k2@gmail.com>
2020-04-30 08:10:15 +0700

    fuck media

--- main/models/userprofile.py
+++ main/models/userprofile.py
@@ -3,12 +3,9 @@ from django.db import models
 
 
 class UserProfile(models.Model):
-    #userprofile = get_user_model()
     user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, related_name='userprofile')
-    #username = models.CharField(userprofile.username, blank=True, null=True, max)
     profile_image = models.ImageField(upload_to='profile_images', default='static/profile_images/masu_profile.jpg',null=True, blank=True)
-    #email = models.EmailField(userprofile.email, blank=True, null=True)
-    #Phone = models.IntegerField(blank=True, null=True)
+
 
     def get_username(self):
         return self.user.username
