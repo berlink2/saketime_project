d719de1e60b688e0405361311b7ab62012eb07ca
berlink2 <berlink.k2@gmail.com>
2020-05-02 17:21:24 +0700

    t

--- saketime_project/settings.py
+++ saketime_project/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['warm-anchorage-76492.herokuapp.com', '127.0.0.1']
 
