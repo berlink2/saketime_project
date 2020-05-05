159bab87bd54455fc36175afef071558a117d0e4
berlink2 <berlink.k2@gmail.com>
2020-05-05 16:45:42 +0700

    finl maybe

--- saketime_project/settings.py
+++ saketime_project/settings.py
@@ -64,12 +64,7 @@ INSTALLED_APPS = [
 
 
 
-# cloudinary.config(
-#     cloud_name = "dw9ogqqqh",
-#     api_key = '455919288359572',
-#     api_secret = '4UxHdABpZXms85D3uvF6Xfy95GA',
-#     secure = True
-# )
+
 
 cloudinary.config(
     cloud_name = env('cloud_name'),
@@ -129,25 +124,25 @@ WSGI_APPLICATION = 'saketime_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'saketime_db',
-#         'USER': 'postgres',
-#         'PASSWORD': 'deadp00l',
-#         'HOST': 'localhost',
-#         'POST': '5432',
-#
-#
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'saketime_db',
+        'USER': 'postgres',
+        'PASSWORD': 'deadp00l',
+        'HOST': 'localhost',
+        'POST': '5432',
+
+
+    }
+}
 
 # import dj_database_url
 #
