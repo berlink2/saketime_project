c4a8bda6a06529c44c660e9b60446889fc65bba4
berlink2 <berlink.k2@gmail.com>
2020-05-09 12:31:10 +0700

    t

--- saketime_project/settings.py
+++ saketime_project/settings.py
@@ -151,9 +151,9 @@ WSGI_APPLICATION = 'saketime_project.wsgi.application'
 
 import dj_database_url
 DATABASES = {}
-#db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
-#DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=600)
+#DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
